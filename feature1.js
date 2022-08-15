0ea0391e246e9ad75d4a8ff1cebce3e01688d703
Richard Opiyo <richardoppiyo@gmail.com>
2022-08-15 11:05:08 +0300

    i have made changes

---
+++ feature1.js
@@ -0,0 +1,26 @@
+function smallestCommon(n) {
+    let arr = n.sort((a,b) => a-b);
+
+    // let newArr = arr.reduce((acc,n) => {
+    //     acc+n
+    // },[])
+    // console.log()
+
+    let ans = arr[1];
+    while(true) {
+        
+        for(let i=arr[1]; i <= arr[1]; i++ ){
+            if( ans % i ) {
+
+            }
+        }
+
+    }
+
+    console.log(arr)
+    
+ CONSOLE.LOG('ADDITONAL CAHNGES')
+
+}
+
+smallestCommon([9,4])
